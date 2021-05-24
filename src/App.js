558948d6cd08b34731b3d55505d1c83cd2746f2f
Johann Hannerth <hannerth94@gmail.com>
2021-05-24 14:57:16 +0100

    sets up basic Reviews component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header-homepage";
 import Nav from "./components/Nav";
+import Reviews from "./components/Reviews";
 
 function App() {
   return (
     <div className="App">
+      {/* <Switch> */}
       <Header />
       <Nav />
+      {/* <Route exact path="/"> */}
+      <Reviews />
+      {/* </Route>
+      </Switch> */}
     </div>
   );
 }
---
+++ src/components/Reviews.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Reviews = () => {
+  return (
+    <body class="reviews">
+      <h2>test</h2>
+      <ul>
+        <li>review one</li>
+        <li>review two</li>
+        <li>review three</li>
+      </ul>
+    </body>
+  );
+};
+
+export default Reviews;
