864d6cd63ba675a8de53ab3827077394e6a02552
Johann Hannerth <hannerth94@gmail.com>
2021-05-24 14:24:46 +0100

    sets up basic header for homepage

---
+++ src/components/Header-homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header class="main_header">
+      <h1> NC GAMES REVIEWS</h1>
+    </header>
+  );
+};
+
+export default Header;
