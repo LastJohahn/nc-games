7472f6d35ece19798d60f550dad963b169eda7fd
Johann Hannerth <hannerth94@gmail.com>
2021-05-28 08:50:04 +0100

    adds UserContext provider

--- src/App.js
+++ src/App.js
@@ -7,38 +7,41 @@ import ReviewById from "./components/ReviewById";
 import Reviews from "./components/Reviews";
 import ReviewsByCategory from "./components/ReviewsByCategory";
 import { getCategories } from "./utils/api";
+import { UserContext } from "./contexts/User";
 
 function App() {
   const [reviews, setReviews] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [user, setUser] = useState([]);
 
   useEffect(() => {
     getCategories().then((result) => {
       const categoriesToUse = result.categories;
-      // console.log(categoriesToUse);
       setCategories(categoriesToUse);
     });
   }, []);
 
   return (
     <div className="App">
-      <Header />
-      <Nav categories={categories} />
-      <Switch>
-        <Route exact path="/">
-          <Reviews reviews={reviews} setReviews={setReviews} />
-        </Route>
-        <Route exact path="/reviews/:review_id">
-          <ReviewById />
-        </Route>
-        <Route exact path="/:category">
-          <ReviewsByCategory
-            reviews={reviews}
-            setReviews={setReviews}
-            categories={categories}
-          />
-        </Route>
-      </Switch>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header />
+        <Nav categories={categories} />
+        <Switch>
+          <Route exact path="/">
+            <Reviews reviews={reviews} setReviews={setReviews} />
+          </Route>
+          <Route exact path="/reviews/:review_id">
+            <ReviewById />
+          </Route>
+          <Route exact path="/:category">
+            <ReviewsByCategory
+              reviews={reviews}
+              setReviews={setReviews}
+              categories={categories}
+            />
+          </Route>
+        </Switch>
+      </UserContext.Provider>
     </div>
   );
 }
