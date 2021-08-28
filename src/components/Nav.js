3bad958b432db7c39a57da73305b4e9d670c9862
Johann Hannerth <hannerth94@gmail.com>
2021-08-28 15:52:38 +0100

    add category buttons toggle

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { UserContext } from "../contexts/User";
 
 const Nav = ({ categories }) => {
   const history = useHistory();
   const { user } = useContext(UserContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   const routeChange = () => {
     let path = `/`;
@@ -16,18 +17,11 @@ const Nav = ({ categories }) => {
     history.push(path);
   };
 
-  // categories as drop down, just map over them below & get them in there
-  // then buttons that redirect there, however that worked
-  // utils function like routechange that takes category as variable and assigns it to path like `/categories/${category}
-  // and then history.push(categoryPath) onclick for each category button
-  // then integrate drop down menu functionality
-
   const userInitials = (user) => {
     const fullName = user.name.split(" ");
     const initials = fullName.shift().charAt(0) + fullName.pop().charAt(0);
     return initials.toUpperCase();
   };
-  console.log(categories);
 
   return (
     <nav className="nav">
@@ -39,17 +33,31 @@ const Nav = ({ categories }) => {
       >
         HOME
       </button>
-      {categories.map((category) => {
-        return (
-          <button
-            onClick={() => {
-              routeChangeCategory(category.slug);
-            }}
-          >
-            {category.slug.replaceAll("-", " ").toUpperCase()}
-          </button>
-        );
-      })}
+      <button
+        className="nav nav__categoryDropDownButton"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        CATEGORIES
+      </button>
+      {isOpen ? (
+        <div className="nav nav__categoryButtons">
+          {categories.map((category) => {
+            return (
+              <button
+                onClick={() => {
+                  routeChangeCategory(category.slug);
+                }}
+              >
+                {category.slug.replaceAll("-", " ").toUpperCase()}
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <p></p>
+      )}
       <div className="nav nav__user">
         {" "}
         <img src={user.avatar_url} />
