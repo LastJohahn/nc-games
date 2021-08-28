245bacaf2d85db778ea211f408ca25afa5eb0a69
Johann Hannerth <hannerth94@gmail.com>
2021-08-28 16:12:50 +0100

    remove 'image' from alt text for picture chosen for review

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -73,7 +73,7 @@ const Reviews = ({ reviews, setReviews }) => {
               <p>{`posted by: ${review.owner}`}</p>
               <img
                 src={review.review_img_url}
-                alt="image reviewer has chosen to represent the game"
+                alt="what the reviewer has chosen to represent the game"
               ></img>
               <p>{review.review_body}</p>
             </li>
