f9699032884be620a40deeb4575edf92b413ec89
Johann Hannerth <hannerth94@gmail.com>
2021-08-31 12:01:58 +0100

    add key prop to WrongPath

--- src/components/WrongPath.js
+++ src/components/WrongPath.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WrongPath = () => {
   return (
-    <div className="wrong_path">
+    <div className="wrong_path" key="wrong_path">
       <h2>Seems like there's nothing here, time to go back!</h2>
     </div>
   );
