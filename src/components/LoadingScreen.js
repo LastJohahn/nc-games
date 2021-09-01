d3ef33af3e1b5c8d8ea3be45558c941094387bf8
Johann Hannerth <hannerth94@gmail.com>
2021-09-01 14:44:11 +0100

    create loadingscreen component

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingScreen = () => {
+  return (
+    <div className="loading_screen" key="loading_screen">
+      <h2>Retrieving data, please hang on a moment!</h2>
+    </div>
+  );
+};
+
+export default LoadingScreen;
