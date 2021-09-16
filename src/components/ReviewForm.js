09b14ff854511ed0870daa640f98be38179857d1
Johann Hannerth <hannerth94@gmail.com>
2021-09-16 13:42:43 +0100

    create basic ReviewForm component

---
+++ src/components/ReviewForm.js
@@ -0,0 +1,8 @@
+import React, { useContext, useState } from "react";
+import { UserContext } from "../contexts/User";
+
+const ReviewForm = () => {
+  return <h2>HI THERE</h2>;
+};
+
+export default ReviewForm;
