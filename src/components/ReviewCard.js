f444d932e8a89b5d510924a9a58d011a1955aac1
Johann Hannerth <hannerth94@gmail.com>
2021-09-18 11:05:46 +0100

    add classname to review for styling

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -4,7 +4,7 @@ import Votes from "./Votes.js";
 
 const ReviewCard = ({ review }) => {
   return (
-    <li key={review.review_id}>
+    <li key={review.review_id} className="reviews">
       <Link to={`/reviews/${review.review_id}`}>
         <h2 className="reviews reviews__title">{review.title}</h2>
       </Link>
