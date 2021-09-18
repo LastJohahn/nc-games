36dbf239ee4b28a273882874a2146c6974ece065
Johann Hannerth <hannerth94@gmail.com>
2021-09-18 10:28:56 +0100

    set up basic form which keeps userinput in state

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,8 +1,77 @@
 import React, { useContext, useState } from "react";
+import { useParams } from "react-router";
 import { UserContext } from "../contexts/User";
 
-const ReviewForm = () => {
-  return <h2>HI THERE</h2>;
+const ReviewForm = ({ categories }) => {
+  const [reviewTitle, setReviewTitle] = useState("");
+  const [reviewBody, setReviewBody] = useState("");
+  const [reviewDesigner, setReviewDesigner] = useState("");
+  const [reviewCategory, setReviewCategory] = useState("");
+  const [isReviewPosted, setIsReviewPosted] = useState(false);
+  const [isReviewError, setIsReviewError] = useState(false);
+
+  return (
+    <div className="reviewForm">
+      <form
+        className="reviewForm__form"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="reviewTitle">REVIEW TITLE</label>
+        <br />
+        <input
+          id="reviewTitle"
+          name="reviewTitle"
+          type="text"
+          onChange={(e) => {
+            setReviewTitle(e.target.value);
+          }}
+        />
+        <br />
+        <label>DESIGNER</label>
+        <br />
+        <input
+          id="reviewDesigner"
+          name="reviewDesigner"
+          type="text"
+          onChange={(e) => {
+            setReviewDesigner(e.target.value);
+          }}
+        />
+        <br />
+        <label htmlFor="reviewCategory">CATEGORY</label>
+        <br />
+        <select
+          id="reviewCategory"
+          name="reviewCategory"
+          onChange={(e) => {
+            setReviewCategory(e.target.value);
+          }}
+        >
+          {categories.map((category) => {
+            return (
+              <option value={category.slug} key={category.slug}>
+                {category.slug}
+              </option>
+            );
+          })}
+        </select>
+        <br />
+        <label htmlFor="reviewBody">REVIEW BODY</label>
+        <br />
+        <textarea
+          id="reviewBody"
+          name="reviewBody"
+          onChange={(e) => {
+            setReviewBody(e.target.value);
+          }}
+        />
+        <br />
+        <input type="submit" value="POST REVIEW"></input>
+      </form>
+    </div>
+  );
 };
 
 export default ReviewForm;
