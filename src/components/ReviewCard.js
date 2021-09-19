10b4c82fa2f2312b504622466ca3134e00963a18
Johann Hannerth <hannerth94@gmail.com>
2021-09-19 12:29:15 +0100

    set up basic button logic for future delete review button

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Votes from "./Votes.js";
+import { UserContext } from "../contexts/User";
 
 const ReviewCard = ({ review }) => {
+  const { user } = useContext(UserContext);
+
   return (
     <li key={review.review_id} className="reviews">
       <Link to={`/reviews/${review.review_id}`}>
         <h2 className="reviews reviews__title">{review.title}</h2>
       </Link>
+      <div>
+        {review.owner === user.username ? (
+          <button className="reviews reviews__reviewsDeleteButton">
+            DELETE COMMENT
+          </button>
+        ) : (
+          <span></span>
+        )}
+      </div>
       {review.comment_count ? (
         <p>{`comments: ${review.comment_count}`}</p>
       ) : (
